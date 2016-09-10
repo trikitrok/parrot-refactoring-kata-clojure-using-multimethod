450b377ac81c730955a7f211bb9ae03594c28a46
trikitrok <trikitrok@gmail.com>
2016-09-11 01:41:48 +0200

    introduce constant to remove magic number

--- src/parrot_refactoring/core.clj
+++ src/parrot_refactoring/core.clj
@@ -2,6 +2,8 @@
 
 (def ^:private base-speed 12.0)
 
+(def ^:private minimum-speed 0.0)
+
 (defmulti speed :type)
 
 (defmethod speed :european-parrot [_]
@@ -9,11 +11,11 @@
 
 (defmethod speed :african-parrot [{:keys [num-coconuts]}]
   (let [load-factor 9.0]
-    (max 0.0 (- base-speed (* load-factor num-coconuts)))))
+    (max minimum-speed (- base-speed (* load-factor num-coconuts)))))
 
 (defmethod speed :norwegian-blue-parrot [{:keys [nailed voltage]}]
   (if nailed
-    0.0
+    minimum-speed
     (min 24.0 (* voltage base-speed))))
 
 (defmethod speed :default [_]
