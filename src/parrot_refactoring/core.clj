a54d42599a27dbb621bbc8036f8b525b66db02fa
trikitrok <trikitrok@gmail.com>
2016-09-11 01:16:13 +0200

    statr parallel change to polymorphism, dispatching on type :european-parrot

--- src/parrot_refactoring/core.clj
+++ src/parrot_refactoring/core.clj
@@ -7,9 +7,14 @@
 (defn- compute-base-speed-for-voltage [voltage]
   (min 24.0 (* voltage base-speed)))
 
+(defmulti speed* :type)
+
+(defmethod speed* :european-parrot [_]
+  base-speed)
+
 (defn speed [parrot]
   (case (:type parrot)
-    :european-parrot base-speed
+    :european-parrot (speed* parrot)
     :african-parrot (max 0.0 (- base-speed (* load-factor (:num-coconuts parrot))))
     :norwegian-blue-parrot (if (:nailed parrot)
                              0.0
