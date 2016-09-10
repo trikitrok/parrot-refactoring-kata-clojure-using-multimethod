7bf9b2ec77ffa3424d10fd514ea2304842d04c65
trikitrok <trikitrok@gmail.com>
2016-09-11 01:39:01 +0200

    remove obsolete function wrapping

--- src/parrot_refactoring/core.clj
+++ src/parrot_refactoring/core.clj
@@ -7,21 +7,18 @@
 (defn- compute-base-speed-for-voltage [voltage]
   (min 24.0 (* voltage base-speed)))
 
-(defmulti speed* :type)
+(defmulti speed :type)
 
-(defmethod speed* :european-parrot [_]
+(defmethod speed :european-parrot [_]
   base-speed)
 
-(defmethod speed* :african-parrot [{:keys [num-coconuts]}]
+(defmethod speed :african-parrot [{:keys [num-coconuts]}]
   (max 0.0 (- base-speed (* load-factor num-coconuts))))
 
-(defmethod speed* :norwegian-blue-parrot [{:keys [nailed voltage]}]
+(defmethod speed :norwegian-blue-parrot [{:keys [nailed voltage]}]
   (if nailed
     0.0
     (compute-base-speed-for-voltage voltage)))
 
-(defmethod speed* :default [_]
+(defmethod speed :default [_]
   (throw (Exception. "Should be unreachable!")))
-
-(defn speed [parrot]
-  (speed* parrot))
